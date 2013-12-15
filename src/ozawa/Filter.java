8d7d808fa938a8ef71f54002f0eef1c8d0da87cd
Laurence Reading <havoc42@gmail.com>
2013-12-15 19:33:03 +0000

    Adding color filtering

--- src/ozawa/Filter.java
+++ src/ozawa/Filter.java
@@ -1,19 +1,34 @@
 package ozawa;
 
+import hexentities.AbstractCard;
+import hexentities.Card;
+import hexentities.ResourceCard;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.List;
 
-import ozawa.enums.Attribute;
+import enums.Attribute;
+import enums.CardType;
+import enums.ColorFlag;
 
 public class Filter {
-	private String filterString;
-	private final EnumSet<Attribute> attributes;
-	
-	Filter(){
-		attributes= EnumSet.noneOf(Attribute.class);
+	private String						filterString;
+	private final EnumSet<Attribute>	attributes;
+	private final EnumSet<ColorFlag>	colors;
+	private final EnumSet<CardType>		cardTypes;
+	private int							minCost;
+	private int							maxCost;
+
+	public Filter() {
+		attributes = EnumSet.noneOf(Attribute.class);
+		colors = EnumSet.allOf(ColorFlag.class);
+		cardTypes = EnumSet.allOf(CardType.class);
+		minCost = -1;
+		maxCost = -1;
 	}
-	
+
 	public String getFilterString() {
 		return filterString;
 	}
@@ -25,31 +40,80 @@ public class Filter {
 	public void addAttribute(Attribute attribute) {
 		attributes.add(attribute);
 	}
-	
+
 	public void removeAttribute(Attribute attribute) {
 		attributes.remove(attribute);
 	}
-	
-	public boolean isActive(Attribute attribute){
+
+	public void addColor(ColorFlag color) {
+		colors.add(color);
+	}
+
+	public void removeColor(ColorFlag color) {
+		colors.remove(color);
+	}
+
+	public void addType(CardType type) {
+		cardTypes.add(type);
+	}
+
+	public void removeType(CardType type) {
+		cardTypes.remove(type);
+	}
+
+	public boolean isActive(Attribute attribute) {
 		return attributes.contains(attribute);
 	}
-	
-	public ArrayList<Card> filter(ArrayList<Card> cards){
-		ArrayList<Card> result = new ArrayList<Card>();
-		for(Card card : cards){
-			boolean include = true;
-			for(Attribute att :attributes){
-				if(!card.attributeFlags.contains(att)){
-					include=false;
-					break;
-				}
+
+	public boolean isActive(CardType type) {
+		return cardTypes.contains(type);
+	}
+
+	public boolean isActive(ColorFlag color) {
+		return colors.contains(color);
+	}
+
+	public ArrayList<AbstractCard> filter(AbstractCard[] cards) {
+		return (filter(Arrays.asList(cards)));
+	}
+
+	public ArrayList<AbstractCard> filter(List<AbstractCard> cards) {
+		ArrayList<AbstractCard> result = new ArrayList<AbstractCard>();
+		for (AbstractCard abstractCard : cards) {
+			if (filterCard(abstractCard)) {
+				result.add(abstractCard);
 			}
-			if(include){
-				result.add(card);
-			}
-			
+
 		}
 		return result;
+	}
+
+	private boolean filterCard(AbstractCard abstractCard) {
+		if (colors.size() != 6) {
+			boolean include = false;
+			if (abstractCard.colorFlags != null) {
+				for (ColorFlag cardColor : abstractCard.colorFlags) {
+					if (colors.contains(cardColor)) {
+						include = true;
+					}
+				}
+			}
+			if (!include)
+				return false;
+		}
+		if (abstractCard instanceof hexentities.Card) {
+			hexentities.Card card;
+			card = (Card) abstractCard;
+			if (!attributes.isEmpty()) {
+				for (Attribute att : attributes) {
+					if (!Arrays.asList(card.attributeFlags).contains(att)) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+
 	}
 
 }
