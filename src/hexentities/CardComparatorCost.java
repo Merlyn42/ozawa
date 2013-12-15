39b95c8ebeca3ca9ebdac0c91f1b23e47e7dd5a1
Laurence Reading <havoc42@gmail.com>
2013-12-15 22:26:17 +0000

    Adding some Card Comparators

---
+++ src/hexentities/CardComparatorCost.java
@@ -0,0 +1,12 @@
+package hexentities;
+
+import java.util.Comparator;
+
+public class CardComparatorCost implements Comparator<Card> {
+
+	@Override
+	public int compare(Card card1, Card card2) {
+		return card1.resourceCost - card2.resourceCost;
+	}
+
+}
