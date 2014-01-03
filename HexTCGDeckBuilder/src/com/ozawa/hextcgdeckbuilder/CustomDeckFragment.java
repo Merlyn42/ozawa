76c9281ba8b423fe4bb41a6db332c51635afbe32
Dave Kerr <dave.kerr@live.ie>
2014-01-03 00:25:59 +0000

    Full image is now working for the gridview on the custom deck

--- HexTCGDeckBuilder/src/com/ozawa/hextcgdeckbuilder/CustomDeckFragment.java
+++ HexTCGDeckBuilder/src/com/ozawa/hextcgdeckbuilder/CustomDeckFragment.java
@@ -192,6 +192,7 @@ public class CustomDeckFragment extends Fragment implements NavigationDrawerFrag
                 Intent i = new Intent(mainActivity.getApplicationContext(), FullImageActivity.class);
                 // passing array index
                 i.putExtra("id",position);
+                i.putExtra("isMaster", false);
                 startActivity(i);
             }
         });
