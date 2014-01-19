40fae681ca30082b2f0737a20ed373eb4fed6933
Laurence Reading <havoc42@gmail.com>
2014-01-19 15:08:17 +0000

    catching oome on fullscreen

--- HexTCGDeckBuilder/src/com/ozawa/hextcgdeckbuilder/hexentities/AbstractCard.java
+++ HexTCGDeckBuilder/src/com/ozawa/hextcgdeckbuilder/hexentities/AbstractCard.java
@@ -18,136 +18,148 @@ import com.ozawa.hextcgdeckbuilder.enums.CardType;
 import com.ozawa.hextcgdeckbuilder.enums.ColorFlag;
 import com.ozawa.hextcgdeckbuilder.util.HexUtil;
 
-
 /**
  * An Abstract Card used to define common fields for all Hex Cards
  * 
  * @author Chad Kinsella
  */
 public abstract class AbstractCard {
-	
+
 	@SerializedName("m_Id")
-	public GlobalIdentifier id;
+	public GlobalIdentifier		id;
 	@SerializedName("m_SetId")
-	public GlobalIdentifier setID;
+	public GlobalIdentifier		setID;
 	@SerializedName("m_Name")
-	public String name;
+	public String				name;
 	@SerializedName("m_CardNumber")
-	public int cardNumber;
+	public int					cardNumber;
 	@SerializedName("m_ColorFlags")
-	public ColorFlag[] colorFlags;
+	public ColorFlag[]			colorFlags;
 	@SerializedName("m_CardImagePath")
-	public String cardImagePath;
+	public String				cardImagePath;
 	@SerializedName("m_CardType")
-	public CardType[] cardType;
+	public CardType[]			cardType;
 	@SerializedName("m_CardSubtype")
-	public String cardSubtype;
+	public String				cardSubtype;
 	@SerializedName("m_CardRarity")
-	public CardRarity cardRarity;
+	public CardRarity			cardRarity;
 	@SerializedName("m_ArtistName")
-	public String artistName;
+	public String				artistName;
 	@SerializedName("m_GameText")
-	public String gameText;
+	public String				gameText;
 	@SerializedName("m_DesignNotes")
-	public String designNotes;
+	public String				designNotes;
 	@SerializedName("m_Unlimited")
-	public Boolean unlimited;
+	public Boolean				unlimited;
 	@SerializedName("m_Tradeable")
-	public Boolean tradeable;
+	public Boolean				tradeable;
 	@SerializedName("m_HasExtendedLayout")
-	public Boolean hasExtendedLayout;
+	public Boolean				hasExtendedLayout;
 	@SerializedName("m_DefaultLayout")
-	public CardLayout defaultLayout;
+	public CardLayout			defaultLayout;
 	@SerializedName("m_ExtendedLayout")
-	public CardLayout extendedLayout;
+	public CardLayout			extendedLayout;
 	@SerializedName("m_ResourceThresholdGranted")
-	public ResourceThreshold[] resourceThresholdGranted;
-    protected Bitmap image;
-    protected Bitmap portrait;
-    protected int cachedImageWidthLimit;
-    
-    public String getID(){
+	public ResourceThreshold[]	resourceThresholdGranted;
+	protected Bitmap			image;
+	protected Bitmap			portrait;
+	protected int				cachedImageWidthLimit;
+
+	public String getID() {
 		return this.id.gUID;
 	}
 
-    /**
-     * Generates the card's image and returns it as a bitmap also caches the image in the Card's image variable and adds this card to the cache queue.
-     * @param context The context to use to retrieve the image.
-     * @return The card's image as a bitmap
-     */
-    
-	public Bitmap getThumbnailCardBitmap(Context context){
-		
-		int maxWidth = HexUtil.getScreenWidth(context)/3;
-		
-        if (image == null || cachedImageWidthLimit !=maxWidth) {
-        	try{
-        	image = getCardBitmap(context, CardTemplate.findCardTemplate(this, false, CardTemplate.getAllTemplates(context)), maxWidth);
-        	}catch(OutOfMemoryError e){
-        		System.err.println("Ran out of memory, dumping some images from the cache");
-        		ImageCache.emergencyDump(CacheType.GridView);
-        		image = getCardBitmap(context, CardTemplate.findCardTemplate(this, false, CardTemplate.getAllTemplates(context)), maxWidth);
-        	}
-        	cachedImageWidthLimit=maxWidth;
-        	ImageCache.getInstance(CacheType.GridView).queueForRemovalFromCache(context,this, ImageType.WithTemplate);
-        }
-         
+	/**
+	 * Generates the card's image and returns it as a bitmap also caches the
+	 * image in the Card's image variable and adds this card to the cache queue.
+	 * 
+	 * @param context
+	 *            The context to use to retrieve the image.
+	 * @return The card's image as a bitmap
+	 */
+
+	public Bitmap getThumbnailCardBitmap(Context context) {
+
+		int maxWidth = HexUtil.getScreenWidth(context) / 3;
+
+		if (image == null || cachedImageWidthLimit != maxWidth) {
+			try {
+				image = getCardBitmap(context, CardTemplate.findCardTemplate(this, false, CardTemplate.getAllTemplates(context)), maxWidth);
+			} catch (OutOfMemoryError e) {
+				System.err.println("Ran out of memory, dumping some images from the cache");
+				ImageCache.emergencyDump(CacheType.GridView);
+				image = getCardBitmap(context, CardTemplate.findCardTemplate(this, false, CardTemplate.getAllTemplates(context)), maxWidth);
+			}
+			cachedImageWidthLimit = maxWidth;
+			ImageCache.getInstance(CacheType.GridView).queueForRemovalFromCache(context, this, ImageType.WithTemplate);
+		}
+
 		return image;
-    }
-	
+	}
+
 	/**
 	 * Generates the card portrait image, scaled for the listview of a deck
 	 * 
 	 * @param mContext
 	 * @return
 	 */
-	public Bitmap getCardPortait(Context mContext){
-		int maxWidth = HexUtil.getScreenWidth(mContext)/8;
-		
-        if (portrait == null || cachedImageWidthLimit !=maxWidth) {
-        	BitmapFactory.Options portraitOptions = new BitmapFactory.Options();
-        	portraitOptions.inSampleSize = 10;
-        	portrait = BitmapFactory.decodeResource(mContext.getResources(), HexUtil.getResourceID(this.cardImagePath, R.drawable.class), portraitOptions);
-        	if(portrait != null){
-	        	Matrix matrix = new Matrix();
-	        	matrix.postScale(0.5f, 0.5f);
-	        	int dimensions = (portrait.getWidth() / 18);
-	        	portrait = Bitmap.createBitmap(portrait, dimensions*3, dimensions, dimensions*12, dimensions*12, matrix, true);
-	        	portrait = Bitmap.createScaledBitmap(portrait, maxWidth, maxWidth, true);
-	        	cachedImageWidthLimit=maxWidth;
-	        	ImageCache.getInstance(CacheType.ListView).queueForRemovalFromCache(mContext,this,ImageType.WithoutTemplate);
-        	}
-        }
-         
+	public Bitmap getCardPortait(Context mContext) {
+		int maxWidth = HexUtil.getScreenWidth(mContext) / 8;
+
+		if (portrait == null || cachedImageWidthLimit != maxWidth) {
+			BitmapFactory.Options portraitOptions = new BitmapFactory.Options();
+			portraitOptions.inSampleSize = 10;
+			portrait = BitmapFactory.decodeResource(mContext.getResources(), HexUtil.getResourceID(this.cardImagePath, R.drawable.class),
+					portraitOptions);
+			if (portrait != null) {
+				Matrix matrix = new Matrix();
+				matrix.postScale(0.5f, 0.5f);
+				int dimensions = (portrait.getWidth() / 18);
+				portrait = Bitmap.createBitmap(portrait, dimensions * 3, dimensions, dimensions * 12, dimensions * 12, matrix, true);
+				portrait = Bitmap.createScaledBitmap(portrait, maxWidth, maxWidth, true);
+				cachedImageWidthLimit = maxWidth;
+				ImageCache.getInstance(CacheType.ListView).queueForRemovalFromCache(mContext, this, ImageType.WithoutTemplate);
+			}
+		}
+
 		return portrait;
 	}
-	
-	
+
 	/**
-	 * called when a card is removed from the cache queue. Should free the bitmap that was cached for this position in the queue.
-	 * @param imageType which type of image to free.
+	 * called when a card is removed from the cache queue. Should free the
+	 * bitmap that was cached for this position in the queue.
+	 * 
+	 * @param imageType
+	 *            which type of image to free.
 	 */
-	public void clearImageCache(ImageType imageType){
-		switch(imageType){
+	public void clearImageCache(ImageType imageType) {
+		switch (imageType) {
 		case WithoutTemplate:
-			portrait=null;
+			portrait = null;
 			break;
 		case WithTemplate:
-			image=null;
+			image = null;
 			break;
-			
+
 		}
 	}
-    
-    public Bitmap getFullscreenCardBitmap(Context context){
-        return getCardBitmap(context, CardTemplate.findCardTemplate(this, true, CardTemplate.getAllTemplates(context)), HexUtil.getScreenWidth(context));
-    }
-    
-    public boolean isTroop(){
-    	return Arrays.asList(this.cardType).contains(CardType.TROOP);
-    }
 
-	public abstract Bitmap getCardBitmap(Context context, CardTemplate template,
-			int maxWidth) ;
+	public Bitmap getFullscreenCardBitmap(Context context) {
+		try {
+			return getCardBitmap(context, CardTemplate.findCardTemplate(this, true, CardTemplate.getAllTemplates(context)),
+					HexUtil.getScreenWidth(context));
+		} catch (OutOfMemoryError e) {
+			System.err.println("Ran out of memory, dumping some images from the cache");
+			ImageCache.emergencyDump(CacheType.GridView);
+			return getCardBitmap(context, CardTemplate.findCardTemplate(this, true, CardTemplate.getAllTemplates(context)),
+					HexUtil.getScreenWidth(context));
+		}
+	}
+
+	public boolean isTroop() {
+		return Arrays.asList(this.cardType).contains(CardType.TROOP);
+	}
+
+	public abstract Bitmap getCardBitmap(Context context, CardTemplate template, int maxWidth);
 
 }
