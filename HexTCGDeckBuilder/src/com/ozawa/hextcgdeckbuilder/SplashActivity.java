64f292dad898e93fb7fef96e19e048e27488228f
Chad Kinsella <ismisechad@gmail.com>
2014-01-23 22:25:22 +0000

    Speed up splash screen, and update socket gem image.

--- HexTCGDeckBuilder/src/com/ozawa/hextcgdeckbuilder/SplashActivity.java
+++ HexTCGDeckBuilder/src/com/ozawa/hextcgdeckbuilder/SplashActivity.java
@@ -1,32 +1,39 @@
 package com.ozawa.hextcgdeckbuilder;
 
+import java.io.IOException;
 import java.io.InputStream;
 
-import com.ozawa.hextcgdeckbuilder.UI.GifView;
-import com.ozawa.hextcgdeckbuilder.database.DatabaseHandler;
+import org.apache.commons.io.IOUtils;
+
+import com.ozawa.hextcgdeckbuilder.UI.GifDecoderView;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+/**
+ * Splash screen for the app
+ */
 public class SplashActivity extends Activity {
 	
-	// Database
-    public DatabaseHandler dbHandler;
+	private GifDecoderView splash;
     private Handler mHandler = new Handler();
     
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		
 		InputStream stream = null;
 		stream = getResources().openRawResource(R.drawable.splash);
-		GifView splash = new GifView(this, stream);
+		splash = new GifDecoderView(this);
+		try {
+			splash.setBytes(IOUtils.toByteArray(stream));
+		} catch (IOException e) {
+		}
 		splash.getRootView().setBackgroundColor(getResources().getColor(android.R.color.black));
-		setContentView(splash);
 		
+		setContentView(splash);
+		splash.startAnimation();
 		mHandler.postDelayed(new Runnable() {
             public void run() {
                 startDeckActivity();
@@ -37,6 +44,7 @@ public class SplashActivity extends Activity {
 	@Override
 	protected void onPause(){
 		super.onPause();
+		splash.stopAnimation();
 		finish();
 	}
 	
